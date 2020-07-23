cd1de2ab6d5a4c2cf2bbc5bd2cd4b4c639adff09
Mathieu <mathieu.huot@gmail.com>
2020-07-23 14:47:23 -0400

    Reduced park data to one area

--- api-refresh.js
+++ api-refresh.js
@@ -6,13 +6,16 @@ const uniqBy = require('lodash.uniqby');
 
 get(`https://montreal.l3.ckan.io/api/3/action/datastore_search?resource_id=f34c3555-c285-4ef3-a55c-f0f5c440ad2d&sort=Nom&limit=3000`, res => {
     let raw = '';
-    res.on('data', data => { raw += data });
+    res.on('data', data => raw = `${raw}${data}`);
     res.on('end', () => {
         const parsed = JSON.parse(raw);
-        const newArray = uniqBy(parsed.result.records, 'NUM_INDEX');
+        const arrondissement = parsed.result.records.reduce((acc, current) => {
+            if(current.GESTION === 'Ahuntsic-Cartierville') acc.push(current);
+            return uniqBy(acc, 'NUM_INDEX');
+        }, []);
         let extract = new Set();
-        newArray.forEach(item => {
-            const newObj = {Nom: item.Nom, GESTION: item.GESTION, NUM_INDEX: item.NUM_INDEX}
+        arrondissement.forEach(parc => {
+            const newObj = {Nom: parc.Nom, GESTION: parc.GESTION, NUM_INDEX: parc.NUM_INDEX}
             extract.add(newObj);
         });
         const str = JSON.stringify([...extract]);
